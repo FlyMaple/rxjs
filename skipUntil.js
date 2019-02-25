ab1bc755ab78aba94d61d23e2bf8f0eb0fddcb36
Skye.Wu <skye.wu@zyxel.com.tw>
2019-02-25 11:22:42 +0800

    skipUntil
    Signed-off-by: Skye.Wu <skye.wu@zyxel.com.tw>

---
+++ skipUntil.js
@@ -0,0 +1,12 @@
+const { zip, timer, of, interval, from } = require('rxjs');
+const { skip, take, filter, skipUntil } = require('rxjs/operators');
+
+
+// skipUntil 和 skip 不同
+// skip 是跳過次數和條件跳過
+// skipUntil 是等待自己的 observable 發出值才不跳過
+const source = interval(1000);
+const example = source.pipe(
+    skipUntil(timer(6000))
+);
+example.subscribe(console.log);
